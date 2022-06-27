ef1061d67fb5b4311bc93f097e29327fcac7c794
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-06-27 08:50:15 -0500

    basic gamemodes

---
+++ code/Gamemodes/BaseGamemode.cs
@@ -0,0 +1,12 @@
+﻿namespace ZombieHorde;
+
+public partial class BaseGamemode : Entity
+{
+	// IDK what I'll use this for yet, but I know I will probably need it one day!
+
+	[Event.Tick.Server]
+	public virtual void Tick()
+	{
+		// empty for now :)
+	}
+}
---
+++ code/Gamemodes/SurvivalGamemode.cs
@@ -0,0 +1,14 @@
+﻿namespace ZombieHorde;
+
+public partial class SurvivalGamemode : BaseGamemode
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+	}
+}
