784cd5f08b0da607a23534a63d46b939da0875b0
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-07-07 00:52:03 -0500

    cleanup ui

--- code/ui/InventoryBar.cs
+++ code/ui/InventoryBar.cs
@@ -10,7 +10,7 @@ public class InventoryBar : Panel
 
 	public InventoryBar()
 	{
-		for ( int i = 0; i < 9; i++ )
+		for ( int i = 0; i < 6; i++ )
 		{
 			var icon = new InventoryIcon( i + 1, this );
 			slots.Add( icon );
