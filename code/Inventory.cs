dad432fb4817e0da553db33780000a3c42492bdf
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-07-15 21:32:28 -0500

    prevent duplicate weapon listings

--- code/Inventory.cs
+++ code/Inventory.cs
@@ -218,6 +218,7 @@ partial class ZomInventory : BaseInventory
 	{
 		if ( Host.IsServer ) return;
 		await Task.Delay( 1 );
+		if ( List.Contains( child ) ) return;
 		var weapon = child as BaseZomWeapon;
 		if ( !weapon.IsValid() ) return;
 		// figure out which weapon we have
