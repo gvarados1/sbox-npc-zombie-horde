e8e3237a9247a6abedeb51b592d2a2111f2b418e
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-07-17 22:39:49 -0500

    try to get models working (didn't work)

--- code/Weapons/SMG.cs
+++ code/Weapons/SMG.cs
@@ -7,7 +7,8 @@
 partial class SMG : BaseZomWeapon
 {
 	public static readonly Model WorldModel = Model.Load( "weapons/rust_smg/rust_smg.vmdl" );
-	public override string ViewModelPath => "weapons/rust_smg/v_rust_smg.vmdl";
+	//public override string ViewModelPath => "weapons/rust_smg/v_rust_smg.vmdl";
+	public override string ViewModelPath => "weapons/djmaesen/fps-animated-smg/fps-animated-smg.vmdl";
 
 	public override float PrimaryRate => 16.0f;
 	public override float SecondaryRate => 1.0f;
