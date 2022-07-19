b7ff595a0650d79445d974ee966d47a9dbd08f0f
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-07-19 14:45:42 -0500

    new guns

---
+++ code/Weapons/M1A.cs
@@ -0,0 +1,122 @@
+﻿namespace ZombieHorde;
+
+
+[Library( "zom_m1a" ), HammerEntity]
+[EditorModel( "weapons/rust_smg/rust_smg.vmdl" )]
+[Title( "M1A" ), Category( "Weapons" )]
+partial class M1A : BaseZomWeapon
+{
+	public static readonly Model WorldModel = Model.Load( "weapons/licensed/hqfpsweapons/fp_equipment/assaultrifles/akm/w_akm.vmdl" );
+	public override string ViewModelPath => "weapons/licensed/hqfpsweapons/fp_equipment/assaultrifles/m1a/v_m1a.vmdl";
+
+	public override float PrimaryRate => 3.0f;
+	public override float SecondaryRate => 1.0f;
+	public override int ClipSize => 15;
+	public override int AmmoMax => 250;
+	public override float ReloadTime => 2f;
+	public override WeaponSlot WeaponSlot => WeaponSlot.Primary;
+	public override float BulletSpread => .05f;
+	public override float ShotSpreadMultiplier => 2.5f;
+	public override string Icon => "weapons/licensed/HQFPSWeapons/Icons/Inventory/Items/Equipment/Icon_M1A.png";
+	public override Color RarityColor => WeaponRarity.Uncommon;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Model = WorldModel;
+		AmmoClip = ClipSize;
+		AmmoReserve = AmmoMax;
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		// slow auto rate, can spam click to fire as fast as possible
+		// this opens us up to cheating with autoclickers but it doesn't really matter in this game
+		return base.CanPrimaryAttack() || Input.Pressed( InputButton.PrimaryAttack );
+	}
+
+	public override void AttackPrimary()
+	{
+		TimeSincePrimaryAttack = 0;
+
+		if ( !TakeAmmo( 1 ) )
+		{
+			DryFire();
+
+			if ( AvailableAmmo() > 0 )
+			{
+				Reload();
+			}
+			return;
+		}
+
+		(Owner as AnimatedEntity).SetAnimParameter( "b_attack", true );
+
+		// Tell the clients to play the shoot effects
+		ShootEffects();
+		PlaySound( "ar3.shoot" );
+		PlaySound( "ar3.shoot.tail" );
+
+		// Shoot the bullets
+		ShootBullet( BulletSpread, 1.5f, 12.0f, 3.0f );
+
+	}
+
+	[ClientRpc]
+	protected override void ShootEffects()
+	{
+		Host.AssertClient();
+
+		Particles.Create( "particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle" );
+		Particles.Create( "particles/pistol_ejectbrass.vpcf", EffectEntity, "ejection_point" );
+
+		ViewModelEntity?.SetAnimParameter( "fire", true );
+		CrosshairLastShoot = 0;
+	}
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		if ( OverridingAnimator ) return;
+		anim.SetAnimParameter( "holdtype", 2 );
+		anim.SetAnimParameter( "aim_body_weight", 1.0f );
+	}
+
+	public override void RenderCrosshair( in Vector2 center, float lastAttack, float lastReload )
+	{
+		// one day I will make my own crosshairs!
+		var draw = Render.Draw2D;
+
+		var color = Color.Lerp( Color.Red, Color.White, lastReload.LerpInverse( 0.0f, 0.4f ) );
+		draw.BlendMode = BlendMode.Lighten;
+		draw.Color = color.WithAlpha( 0.2f + CrosshairLastShoot.Relative.LerpInverse( 1.2f, 0 ) * 0.5f );
+
+		// center circle
+		{
+			var shootEase = Easing.EaseInOut( lastAttack.LerpInverse( 0.1f, 0.0f ) );
+			var length = 2.0f + shootEase * 2.0f;
+			draw.Circle( center, length );
+		}
+
+
+		draw.Color = draw.Color.WithAlpha( draw.Color.a * 0.2f );
+
+		// outer lines
+		{
+			//var shootEase = Easing.EaseInOut( lastAttack.LerpInverse( 0.2f, 0.0f ) );
+			var shootEase = SpreadMultiplier * 1f;
+			//var length = 3.0f + shootEase * 2.0f;
+			var length = 3.0f + shootEase * 2.0f;
+			//var gap = 30.0f + shootEase * 50.0f;
+			var gap = -5.0f + shootEase * 22.0f;
+			var thickness = 2.0f;
+
+			draw.Line( thickness, center + Vector2.Up * gap + Vector2.Left * length, center + Vector2.Up * gap - Vector2.Left * length );
+			draw.Line( thickness, center - Vector2.Up * gap + Vector2.Left * length, center - Vector2.Up * gap - Vector2.Left * length );
+
+			draw.Line( thickness, center + Vector2.Left * gap + Vector2.Up * length, center + Vector2.Left * gap - Vector2.Up * length );
+			draw.Line( thickness, center - Vector2.Left * gap + Vector2.Up * length, center - Vector2.Left * gap - Vector2.Up * length );
+		}
+	}
+
+}
