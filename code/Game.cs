30503b6aabfc78766536e2258b025432e0673de4
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-07-30 03:04:11 -0500

    adjust low-hp postprocessing

--- code/Game.cs
+++ code/Game.cs
@@ -188,7 +188,7 @@ partial class ZombieGame : Game
 			}
 
 
-			var healthDelta = localPlayer.Health.LerpInverse( 0, 90.0f, true ); // start lowhp effects at 90 instead of 100
+			var healthDelta = localPlayer.Health.LerpInverse( 0, 80.0f, true ); // start lowhp effects at 90 instead of 100
 			if ( localPlayer.LifeState == LifeState.Dying ) healthDelta = 0;
 
 			healthDelta = MathF.Pow( healthDelta, 0.5f );
