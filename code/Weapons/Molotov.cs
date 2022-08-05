6b6705904d59046c73bd00939019d8e1717ebb05
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-08-05 00:54:52 -0500

    molotov (no fire yet)

---
+++ code/Weapons/Molotov.cs
@@ -0,0 +1,129 @@
+﻿using Sandbox;
+
+namespace ZombieHorde;
+
+/// <summary>
+/// Alerts Zombies, Explodes.
+/// </summary>
+[Library( "zom_molotov" ), HammerEntity]
+[EditorModel( "weapons/licensed/hqfpsweapons/fp_equipment/throwables/molotovcocktail/w_molotov.vmdl" )]
+[Title( "Molotov Cocktail" ), Category( "Grenades" )]
+partial class Molotov : BaseZomWeapon
+{
+	public static readonly Model WorldModel = Model.Load( "weapons/licensed/hqfpsweapons/fp_equipment/throwables/molotovcocktail/w_molotov.vmdl" );
+	public override string ViewModelPath => "weapons/licensed/hqfpsweapons/fp_equipment/throwables/molotovcocktail/v_molotov.vmdl";
+
+	public override float PrimaryRate => 1.0f;
+	public override float SecondaryRate => 1.0f;
+	public override float ReloadTime => 1.0f;
+	public override int ClipSize => 1;
+	public override WeaponSlot WeaponSlot => WeaponSlot.Grenade;
+	public override int AmmoMax => 100;
+	public override string Icon => "weapons/licensed/HQFPSWeapons/Icons/Inventory/Items/Equipment/Icon_MolotovCocktail.png";
+	public override Color RarityColor => WeaponRarity.Rare;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Model = WorldModel;
+		AmmoClip = ClipSize;
+		AmmoReserve = AmmoMax;
+	}
+
+	public override bool CanPrimaryAttack()
+	{
+		return Input.Released( InputButton.PrimaryAttack );
+	}
+
+	public override async void AttackPrimary()
+	{
+		TimeSincePrimaryAttack = 0;
+
+		if ( Owner is not HumanPlayer player ) return;
+
+		//if ( !TakeAmmo( 1 ) )
+		if ( false )
+		{
+			Reload();
+			return;
+		}
+
+		// woosh sound
+		// screen shake
+
+		//PlaySound( "dm.grenade_throw" );
+		ViewModelEntity?.SetAnimParameter( "fire", true );
+		PlaySound( "rust_boneknife.attack" );
+		//PlaySound( "grenade.pinpull" );
+		PlaySound( "pipebomb.activate" );
+
+		player.SetAnimParameter( "b_attack", true );
+
+		Rand.SetSeed( Time.Tick );
+
+		await Task.Delay( 300 );
+
+		if ( IsServer )
+			using ( Prediction.Off() )
+			{
+				var grenade = new ThrownMolotov
+				{
+					Position = Owner.EyePosition + Owner.EyeRotation.Forward * 3.0f + Owner.EyeRotation.Right * 5f,
+					Owner = Owner
+				};
+
+				grenade.PhysicsBody.Velocity = Owner.EyeRotation.Forward * 1000.0f + Owner.EyeRotation.Up * 150.0f + Owner.Velocity.WithZ(0);
+				grenade.PhysicsBody.AngularVelocity = (Vector3.Random * 20);
+			}
+
+		await Task.Delay( 100 );
+
+		player.SwitchToBestWeapon();
+		Reload();
+
+		if ( IsServer && AmmoClip == 0 && AmmoReserve == 0 )
+		{
+			Delete();
+			player.SwitchToBestWeapon();
+		}
+	}
+
+	public override void SetCarryPosition()
+	{
+		// dumb hard-coded positions
+		EnableDrawing = true;
+		var transform = Transform.Zero;
+		transform.Position += Vector3.Right * 5;
+		transform.Position += Vector3.Up * -4;
+		transform.Position += Vector3.Forward * -3;
+		transform.Rotation *= Rotation.FromPitch( 0 );
+		transform.Rotation *= Rotation.FromRoll( 270 );
+		SetParent( Owner, "leg_upper_L", transform );
+	}
+
+	public override void RenderCrosshair( in Vector2 center, float lastAttack, float lastReload )
+	{
+		var draw = Render.Draw2D;
+
+		var shootEase = SpreadMultiplier * 1f;
+		var color = Color.Lerp( Color.Red, Color.White, lastReload.LerpInverse( 0.0f, 0.4f ) );
+
+		draw.BlendMode = BlendMode.Lighten;
+		draw.Color = color.WithAlpha( 0.2f + CrosshairLastShoot.Relative.LerpInverse( 1.2f, 0 ) * 0.5f );
+
+		//var length = 3.0f + shootEase * 5.0f;
+		var length = 3.0f + shootEase * 5.0f;
+
+		draw.Ring( center, length, length - 3.0f );
+	}
+
+	public override void SimulateAnimator( PawnAnimator anim )
+	{
+		if ( OverridingAnimator ) return;
+		anim.SetAnimParameter( "holdtype", 4 );
+		anim.SetAnimParameter( "aimat_weight", 1.0f );
+		anim.SetAnimParameter( "holdtype_attack", 2.0f );
+		anim.SetAnimParameter( "holdtype_handedness", 1 );
+	}
+}
---
+++ code/Weapons/ThrownMolotov.cs
@@ -0,0 +1,85 @@
+﻿namespace ZombieHorde;
+
+partial class ThrownMolotov : BasePhysics
+{
+	public static readonly Model WorldModel = Model.Load( "weapons/licensed/hqfpsweapons/fp_equipment/throwables/molotovcocktail/w_molotov.vmdl" );
+
+	Particles GrenadeParticles;
+	private TimeSince TimeSinceBeeped = 0;
+
+	public override void Spawn()
+	{
+		base.Spawn();
+		Tags.Add( "Grenade" );
+
+		Model = WorldModel;
+		SetupPhysicsFromModel( PhysicsMotionType.Dynamic );
+		EnableTouch = true;
+
+		GrenadeParticles = Particles.Create( "particles/grenade.vpcf", this, "trail_particle", true );
+		GrenadeParticles.SetPosition( 0, Position );
+
+		// explode in 10 seconds regardless
+		_ = BlowIn( 10 );
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+		if ( other != Owner )
+		{
+			_ = BlowIn( 0 );
+		}
+	}
+
+	public async Task BlowIn( float seconds )
+	{
+		await Task.DelaySeconds( seconds );
+
+		if ( !IsValid ) return;
+
+		Sound.FromWorld( "molotov.break", Position );
+		Explode( this, Owner, Position, 700, 100, 1.5f );
+		Delete();
+	}
+
+	public void Explode( Entity weapon, Entity owner, Vector3 position, float radius, float damage, float forceScale )
+	{
+		Particles.Create( "particles/explosion/barrel_explosion/explosion_barrel.vpcf", position );
+
+		// same thing as regular explosion but only zombies, don't check los, and don't gib
+		var overlaps = Entity.FindInSphere( position, radius ).OfType<CommonZombie>();
+
+		foreach ( var overlap in overlaps )
+		{
+			if ( overlap is not ModelEntity ent || !ent.IsValid() )
+				continue;
+
+			if ( ent.LifeState != LifeState.Alive )
+				continue;
+
+			if ( !ent.PhysicsBody.IsValid() )
+				continue;
+
+			if ( ent.IsWorld )
+				continue;
+
+			var targetPos = ent.PhysicsBody.MassCenter;
+
+			var dist = Vector3.DistanceBetween( position, targetPos );
+			if ( dist > radius )
+				continue;
+
+			var distanceMul = 1.0f - Math.Clamp( dist / radius, 0.0f, 1.0f );
+			var dmg = damage * distanceMul;
+			var force = (forceScale * distanceMul) * ent.PhysicsBody.Mass;
+			var forceDir = (targetPos - position - Vector3.Down*80).Normal;
+
+			var damageInfo = DamageInfo.FromBullet( position, forceDir * force, dmg )
+				.WithWeapon( weapon )
+				.WithAttacker( owner );
+
+			ent.TakeDamage( damageInfo );
+		}
+	}
+}
