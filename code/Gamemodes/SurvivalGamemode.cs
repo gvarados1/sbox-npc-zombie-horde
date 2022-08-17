26812853407c1be8409482339c0b452bfcd0ed7d
Gvarados <39461072+gvarados1@users.noreply.github.com>
2022-08-17 04:21:46 -0500

    expire flames on round end

--- code/Gamemodes/SurvivalGamemode.cs
+++ code/Gamemodes/SurvivalGamemode.cs
@@ -228,6 +228,9 @@ public partial class SurvivalGamemode : BaseGamemode
 
 			foreach ( var item in Entity.All.OfType<Tripmine>().ToArray() )
 				item.Delete();
+
+			foreach ( var item in Entity.All.OfType<Flames>().ToArray() )
+				item.Expire();
 		}
 		
 		foreach ( var item in Entity.All.OfType<PingMarker>().ToArray() )
